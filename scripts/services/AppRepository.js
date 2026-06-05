b54194b37539d84a4f9419608eea1a7b52c17a91
Hoeleboele <lucsomers@hotmail.com>
2026-06-05 15:09:38 +0200

    Move logo assets into logos folder and update paths

--- scripts/services/AppRepository.js
+++ scripts/services/AppRepository.js
@@ -6,13 +6,13 @@ export class AppRepository {
 			new AppEntry({
 				name: "DND Character Tracker",
 				url: "https://hoeleboele.github.io/DNDCharacterTracker/",
-				logoPath: "DndCharacterTrackerLogo.png",
+				logoPath: "logos/DndCharacterTrackerLogo.png",
 				description: "Track your D&D characters, stats, and inventory. Made for use during Dungeons and Dragons sessions."
 			}),
 			new AppEntry({
 				name: "GOA Init Tracker",
 				url: "https://hoeleboele.github.io/GOAInitTracker/",
-				logoPath: "GuardsInitiativeTrackerLogo.png",
+				logoPath: "logos/GuardsInitiativeTrackerLogo.png",
 				description: "Manage initiative order for Guards of Atlantis 2 the boardgame."
 			})
 		];

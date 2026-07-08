21414ab34f27616848c0364d62ae423bb401290c
Hoeleboele <lucsomers@hotmail.com>
2026-07-08 11:46:59 +0200

    Add new app entries for Boardgame Helper and 6 Siege Timer with logos

--- scripts/services/AppRepository.js
+++ scripts/services/AppRepository.js
@@ -16,6 +16,18 @@ export class AppRepository {
 				description: "Manage initiative order for Guards of Atlantis 2 the boardgame."
 			}),
 			new AppEntry({
+				name: "Boardgame Helper",
+				url: "https://hoeleboele.github.io/UniversalBoardgameHelper/",
+				logoPath: "logos/BoardgameHelperLogo.png",
+				description: "Assist with various boardgames, keeping track of scores, lives or choose a start player."
+			}),
+			new AppEntry({
+				name: "6 Siege Timer",
+				url: "https://hoeleboele.github.io/6-Siege-Timer/",
+				logoPath: "logos/6SiegeTimerLogo.png",
+				description: "A timer for the boardgame 6 Siege, to manage the game flow and keep track of the time left for each player."
+			}),
+			new AppEntry({
 				name: "Iron Quest",
 				url: "https://hoeleboele.github.io/FitnessTracker/",
 				logoPath: "logos/IronQuestLogo.png",
